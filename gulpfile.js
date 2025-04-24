f5d338f05efe73cc6bd461e4d1f2bcd00402fb80
matheus-chagas <matheus.fernando.chagas@outlook.com.br>
2025-04-24 13:36:55 -0300

    Alteração da versão do gulp

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,7 @@ function styles() {
 function images() {
     return gulp.src('./src/images/**/*')
         .pipe(imagemin())
-        .pipe(gulp.dest('./dist/images')); // Esse código executado ele cria a minificação das imagens"\\
+        .pipe(gulp.dest('./dist/images'));
 }
 
 exports.default = gulp.parallel(styles, images, scripts);
